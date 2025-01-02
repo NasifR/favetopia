b821fafbbf44065dd6df93fe3c0b4af32b84ec0e
unknown <nasifjuhayer@gmail.com>
2025-01-02 14:41:25 -0500

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='w-full h-20 bg-purple-700 sticky top-0'>
+        <div className='container mx-auto px-4 h-full'>
+            <div className='flex justify-between items-center h-full'>
+                {/* <Logo/> */}
+                <ul></ul>
+
+            </div>
+
+        </div>
+    </div>
+        
+    </>
+  )
+}
+
+export default Navbar
