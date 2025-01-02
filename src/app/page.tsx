2f4a8aa4b342cf26abe39849d9739329df4e8960
unknown <nasifjuhayer@gmail.com>
2025-01-02 15:11:11 -0500

    navbar style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Navbar from '@/components/navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <nav>
-      <Navbar/>
-    </nav>
+    <div>Home page</div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,39 +4,39 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-purple-700 sticky top-0">
+      <div className="w-full h-16 bg-purple-700 sticky top-0">
         <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
+          <div className="flex justify-center items-center h-full">
             {/* <Logo/> */}
-            <ul className="hidden justify-between md:flex gap-x-6 text-white">
+            <ul className="hidden md:flex gap-x-16 text-white text-xl">
               <li>
                 <Link href="/">
-                  <p>Home</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">Home</p>
                 </Link>
               </li>
               <li>
                 <Link href="/games">
-                  <p>Games</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">Games</p>
                 </Link>
               </li>
               <li>
                 <Link href="/movies">
-                  <p>Movies</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">Movies</p>
                 </Link>
               </li>
               <li>
                 <Link href="/anime">
-                  <p>Anime</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">Anime</p>
                 </Link>
               </li>
               <li>
                 <Link href="/shows">
-                  <p>TV Shows</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">TV Shows</p>
                 </Link>
               </li>
               <li>
                 <Link href="/books">
-                  <p>Books</p>
+                  <p className=" hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">Books</p>
                 </Link>
               </li>
             </ul>
