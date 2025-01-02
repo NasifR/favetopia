d3b025eb12d1b7b6551ac0c8f533becf73bc3d38
unknown <nasifjuhayer@gmail.com>
2025-01-02 14:49:23 -0500

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-    <div className='w-full h-20 bg-purple-700 sticky top-0'>
-        <div className='container mx-auto px-4 h-full'>
-            <div className='flex justify-between items-center h-full'>
-                {/* <Logo/> */}
-                <ul></ul>
-
-            </div>
-
+      <div className="w-full h-20 bg-purple-700 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            {/* <Logo/> */}
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/">
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/games">
+                  <p>Games</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/movies">
+                  <p>Movies</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/anime">
+                  <p>Anime</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/shows">
+                  <p>TV Shows</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/books">
+                  <p>Books</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
-        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
