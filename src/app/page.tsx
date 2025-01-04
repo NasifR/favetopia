f6b99650089e1d21d2f622b7b7fe5b753d902801
unknown <nasifjuhayer@gmail.com>
2025-01-03 19:12:51 -0500

    heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ import "./home.css"
 
 const page = () => {
   return (
+    <main>
     <div>
       <Image src={bg} alt="Cover Image" className="bg-img" width={3500} height={1969}  />
     </div>
+    <div className='mt-20 py-10 px-10 ml-10'>
+      <h1 className='text-8xl h-[30rem]'>FAVETOPIA</h1>
+    </div>
+    </main>
 
   )
 }
