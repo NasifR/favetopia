3a6ceeeeafa5364748c43fe5cd7d5205078b682d
unknown <nasifjuhayer@gmail.com>
2025-01-05 14:08:04 -0500

    page bg

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+import bg from "../../assets/movies.jpg"
 
 const movies = () => {
   return (
-    <div>Welcome to movie's page</div>
+    <main>
+    <div>
+      <Image src={bg} alt="Cover Image" className="bg-img" width={1920} height={1080}  />
+    </div>
+    </main>
   )
 }
 
--- src/app/shows/page.tsx
+++ src/app/shows/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 const shows = () => {
   return (
-    <div>Welcome to shows's page</div>
+    <main>
+    <div>
+      <Image src={bg} alt="Cover Image" className="bg-img" width={1920} height={1080}  />
+    </div>
+    </main>
   )
 }
 
