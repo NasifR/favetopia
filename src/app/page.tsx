7718d78d3fe2fb61a8c3b7c1da846863e04d3b0f
unknown <nasifjuhayer@gmail.com>
2025-01-07 15:02:12 -0500

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,15 @@ const page = () => {
     <div>
       <Image src={bg} alt="Cover Image" className="bg-img" width={3500} height={1969}  />
     </div>
-    <div className='mt-20 py-1 px-10 ml-10'>
-      <h1 className='text-8xl'>FAVETOPIA</h1>
+    <div className='mt-40 py-1 px-10 ml-10'>
+      <h1 className='text-8xl ml-20'>FAVETOPIA</h1>
     </div>
     <div className='ml-20'>
-      <h2 className='text-gray-300 text-3xl'>All your favorites, in one place!</h2>
+      <h2 className='text-gray-300 text-3xl ml-20 mt-4'>All your favorites, in one place!</h2>
     </div>
     <div>
       <Link href="/about">
-      <button className='ml-20 px-3 py-3 mt-2 text-white bg-purple-700 rounded-md hover:bg-black transition-all'>Learn More</button>
+      <button className='ml-40 px-3 py-3 mt-6 text-white bg-purple-700 rounded-md hover:bg-black transition-all'>Learn More</button>
       </Link>
     </div>
     </main>
