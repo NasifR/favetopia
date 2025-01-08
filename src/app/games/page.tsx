c8d0784d0ff39cc33555994e4f822fba04ffb4dd
unknown <nasifjuhayer@gmail.com>
2025-01-08 16:04:31 -0500

    form

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -2,16 +2,36 @@
 import React from 'react'
 import Image from 'next/image'
 import bg from "../../assets/games.jpg"
+import ModalForm from '@/components/ModalForm';
+import { useState } from "react";
 
 const games = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  const handleFormSubmit = (formData: any) => {
+    console.log("Form submitted:", formData);
+    alert(`New item added:\nTitle: ${formData.title}\nRating: ${formData.rating}\nStatus: ${formData.status}\nCover: ${formData.cover}`);
+  };
+
   return (
     <main>
     <div>
       <Image src={bg} alt="Cover Image" className="bg-img" width={1920} height={1080}  />
     </div>
-    <div>
-      
-    </div>
+    <button
+        onClick={handleOpenModal}
+        className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600"
+      >
+        Add New Item
+      </button>
+
+      {isModalOpen && (
+        <ModalForm onClose={handleCloseModal} onSubmit={handleFormSubmit} />
+      )}
     </main>
   )
 }
