0f7f07670fef04e31c5802de5eb33006129cde19
unknown <nasifjuhayer@gmail.com>
2025-01-08 15:50:20 -0500

    form

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,12 +1,17 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
 import bg from "../../assets/games.jpg"
+import Form from '@/components/Form'
 
 const games = () => {
   return (
     <main>
     <div>
       <Image src={bg} alt="Cover Image" className="bg-img" width={1920} height={1080}  />
+    </div>
+    <div>
+      <Form/>
     </div>
     </main>
   )
