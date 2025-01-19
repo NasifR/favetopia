d52ccddaf895776b0f57600c7afd680f9a7b41c4
unknown <nasifjuhayer@gmail.com>
2025-01-18 23:56:53 -0500

    form

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,13 +1,36 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
 import bg from "../../assets/anime.png"
-
+import Form from '@/components/Form';
+import { useState } from "react";
 const anime = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const handleOpenModal = () => setIsModalOpen(true);
+    const handleCloseModal = () => setIsModalOpen(false);
+  
+    const handleFormSubmit = (formData: any) => {
+      console.log("Form submitted:", formData);
+      alert(`New item added:\nTitle: ${formData.title}\nRating: ${formData.rating}\nStatus: ${formData.status}\nCover: ${formData.cover}`);
+    };
+
   return (
     <main>
     <div>
       <Image src={bg} alt="Cover Image" className="bg-img" width={1920} height={1080}  />
     </div>
+    <button
+        onClick={handleOpenModal}
+        className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600"
+      >
+        Add New Item
+      </button>
+
+      {isModalOpen && (
+        <Form onClose={handleCloseModal} onSubmit={handleFormSubmit} />
+      )}
     </main>
   )
 }
