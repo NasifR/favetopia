906c1acd83cd331bbc7d2721d6ca66d97c7a048b
unknown <nasifjuhayer@gmail.com>
2025-01-21 12:47:22 -0500

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = () => {
       <div className="w-full h-16 bg-purple-700 sticky top-0">
         <div className="container mx-auto px-4 h-full relative">
           <div className="flex justify-center items-center h-full">
-            {/* Centered Links */}
-            <ul className="hidden md:flex gap-x-16 text-white text-xl">
+
+            <ul className="hidden md:flex gap-x-12 text-white text-xl">
               <li>
                 <Link href="/">
                   <p className="hover:text-purple-700 hover:bg-white transition-all py-1 px-1 rounded-sm">
@@ -57,7 +57,6 @@ const Navbar = () => {
               </li>
             </ul>
 
-            {/* Right-Aligned Button */}
             <div className="absolute right-4 text-white">
               {user ? (
                 <button
@@ -68,7 +67,7 @@ const Navbar = () => {
                 </button>
               ) : (
                 <Link href="/signup">
-                  <button className="hover:text-purple-700 hover:bg-white transition-all py-1 px-4 rounded-md">
+                  <button className="hover:text-purple-700 border border-white hover:bg-white transition-all py-1 px-4 rounded-full">
                     Login
                   </button>
                 </Link>
