250f5ea99716ffb1af2efeff60276c163802b5e4
unknown <nasifjuhayer@gmail.com>
2025-01-26 19:14:19 -0500

    with auth hoc

---
+++ src/components/withAuth.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../lib/firebaseConfig"; // Your Firebase config path
+
+const withAuth = (WrappedComponent: React.ComponentType) => {
+  return (props: any) => {
+    const router = useRouter();
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          router.replace("/login");
+        }
+      });
+
+      return () => unsubscribe();
+    }, [router]);
+
+    return <WrappedComponent {...props} />;
+  };
+};
+
+export default withAuth;
