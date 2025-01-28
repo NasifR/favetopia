ff51b744752a6704cb862830a758d0de08ac453c
unknown <nasifjuhayer@gmail.com>
2025-01-27 19:17:00 -0500

    config env

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHglVWjJMfBn_9UejllYQNtQpUP3Hw7RM",
-  authDomain: "favetopia.firebaseapp.com",
-  projectId: "favetopia",
-  storageBucket: "favetopia.firebasestorage.app",
-  messagingSenderId: "644345267065",
-  appId: "1:644345267065:web:cc15a84c78d1f63a9925a8",
-  measurementId: "G-EZ07P5SPET"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
